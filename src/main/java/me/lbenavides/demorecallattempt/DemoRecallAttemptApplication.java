152173759959cc3dab80ad7f0f76cac8f7d92093
Luis <luisalejobg@gmail.com>
2024-02-20 14:39:27 -0300

    first commit

---
+++ src/main/java/me/lbenavides/demorecallattempt/DemoRecallAttemptApplication.java
@@ -0,0 +1,13 @@
+package me.lbenavides.demorecallattempt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoRecallAttemptApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoRecallAttemptApplication.class, args);
+    }
+
+}
